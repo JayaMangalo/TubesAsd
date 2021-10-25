3fdfbe93ceb7caa03fc6f83cad595126cdc28764
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-10-25 21:07:54 +0700

    first commit in main menu

---
+++ src/mainmenu.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mainmenu() {
+    printf("Selamat datang di Game Mobita.\n");
+    printf("Silakan pilih command berikut ini:\n");
+    printf("1.NEW GAME\n2.EXIT\n3.LOAD GAME\n");
+}
+
+int main () {
+    mainmenu();
+}
