ac6c78d777e5aa2168174eed685740c0bf384c88
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-10-31 05:42:53 +0700

    push boolean file in all ADT folders

--- src/mainmenu.c
+++ src/mainmenu.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../src/ADT Mesin Kata/wordmachine.h"
 
 void mainmenu() {
-    printf("Selamat datang di Game Mobita.\n");
+    printf("MAIN MENU");
     printf("Silakan pilih command berikut ini:\n");
     printf("1.NEW GAME\n2.EXIT\n3.LOAD GAME\n");
+
 }
 
 int main () {
