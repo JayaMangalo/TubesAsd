cde9412880d6e2faf93b603990d38fb09bb81a63
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-10 22:02:13 +0700

    main menu almost done, sudah bisa baca file, sudah bisa baca command, sudah bisa validasi nama file

--- src/setup_driver.c
+++ src/setup_driver.c
@@ -46,7 +46,31 @@ int main(){
     startCommand();
     if (isEqualCommand(currentCommand,NEW_GAMES)) {
         printf("INPUT NAMA FILE: ");
+        Command FILE_CONFIGURATION_NAME;
+        FILE_CONFIGURATION_NAME.contents[0] = 'c';
+        FILE_CONFIGURATION_NAME.contents[1] = 'o';
+        FILE_CONFIGURATION_NAME.contents[2] = 'n';
+        FILE_CONFIGURATION_NAME.contents[3] = 'f';
+        FILE_CONFIGURATION_NAME.contents[4] = 'i';
+        FILE_CONFIGURATION_NAME.contents[5] = 'g';
+        FILE_CONFIGURATION_NAME.contents[6] = 'u';
+        FILE_CONFIGURATION_NAME.contents[7] = 'r';        
+        FILE_CONFIGURATION_NAME.contents[8] = 'a';
+        FILE_CONFIGURATION_NAME.contents[9] = 't';
+        FILE_CONFIGURATION_NAME.contents[10] = 'i';
+        FILE_CONFIGURATION_NAME.contents[11] = 'o';
+        FILE_CONFIGURATION_NAME.contents[12] = 'n';
+        FILE_CONFIGURATION_NAME.contents[13] = '.';
+        FILE_CONFIGURATION_NAME.contents[14] = 't';
+        FILE_CONFIGURATION_NAME.contents[15] = 'x';
+        FILE_CONFIGURATION_NAME.contents[16] = 't';
+        FILE_CONFIGURATION_NAME.length = 17;
         startCommand();
+        while(!isEqualCommand(currentCommand,FILE_CONFIGURATION_NAME)) {
+            printf("NAMA FILE SALAH, SILAKAN INPUT ULANG!\n");
+            printf("INPUT NAMA FILE: ");
+            startCommand();
+        }
         setup();
     } else if(isEqualCommand(currentCommand,LOAD_GAMES)) {
         printf("INPUT NAMA FILE: ");
