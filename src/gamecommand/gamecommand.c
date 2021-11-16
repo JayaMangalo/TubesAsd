4e47f6ee00785ef79cdc9952840feb57f604b098
ilhampw <ilhamprasetyowb@gmail.com>
2021-11-16 12:27:40 +0700

    revisi dropoff

--- src/gamecommand/gamecommand.c
+++ src/gamecommand/gamecommand.c
@@ -17,15 +17,15 @@ void CommandMove(){
     //donothing
 }
 void CommandPickUp(){           //blomselesai
-    if (isStackFull(Tas)){
-        printf("Tas Sudah Full, Tidak Bisa Melakukan Pick UP.");
-    }
-    else{
-        //
-    }
+    // if (isStackFull(Tas)){
+    //     printf("Tas Sudah Full, Tidak Bisa Melakukan Pick UP.");
+    // }
+    // else{
+    //     //
+    // }
 }
 
-void CommandDropOff(List *inprog, stack *tas, char loc){
+void CommandDropOff(List *inprog, Stack *tas, char loc){
     /* KAMUS LOKAL */
     Address current = FIRST(*inprog);
     int i = 0;
@@ -37,9 +37,10 @@ void CommandDropOff(List *inprog, stack *tas, char loc){
     }
 
     if (DropOff(INFO(current)) == loc) {
-        if (INFO(current) == TOP(*tas)) { // masih belum bisa ngebandingin item di tas sama di order
-            pop(*tas,&ord);
-            deleteAt(*inprog,i,&ord);
+        if (INFO(current) == TOP(*tas)) { // udah dites, tinggal ngebandingin dua order sama atau ngga
+            popStack(tas,&ord);
+            deleteAt(inprog,i,&ord);
+            printf("Barang berhasil diantarkan pada lokasi %c.\n", loc);
         }
         else {
             printf("Item teratas pada tas tidak sesuai dengan pesanan pada lokasi.\n");
@@ -67,15 +68,17 @@ void CommandInventory(){
 }
 void CommandHelp(){
     printf(" ----------------------HELP------------------\n");
-    printf("LIST COMMAND YANG BISA DIMASUKKAN\n");
+    printf("\n");
     printf("1. MOVE : Menampilkan pilihan lokasi yang dapat dicapai dan berpindah ke tempat pilihan\n");
-    printf("2. PICK_UP : Mengambil item pada lokasi dan menghapusnya dari To-Do list.")
-    printf("3. DROP_OFF : Mengantarkan item pada lokasi.");
-    printf("4. MAP : Menampilkan peta.");
+    printf("2. PICK_UP : Mengambil item pada lokasi dan menghapusnya dari To-Do list.\n");
+    printf("3. DROP_OFF : Mengantarkan item pada lokasi.\n");
+    printf("4. MAP : Menampilkan peta.\n");
     printf("5. TO_DO : Menampilkan pesanan yang masuk pada To Do list.\n");
     printf("6. BUY : Digunakan untuk menampilkan gadget yang dapat dibeli. Hanya dapat dipanggil di HQ.\n");
     printf("7. INVENTORY : Menampilkan isi list inventory (gadgets) dan bisa memilih menggunakan gadget.\n");
     printf("8. HELP : Menampilkan list command dan penjelasannya.\n");
+    printf("\n");
+    printf("----------------------------------------------\n");
 }
 boolean CommandExit(){
     printf("ARE YOU SURE YOU WANT TO EXIT THE GAME? YOUR PROGRESS WILL NOT BE SAVED\n");
