64e4e88eb74f2bdedb0c0b554582c2ac2514a313
Leoni Antoinette <leoniantoinette12@gmail.com>
2021-11-16 11:41:00 +0700

    update command map

--- RawSrc/command map/commandmap.c
+++ RawSrc/command map/commandmap.c
@@ -1,48 +1,13 @@
 #include "map\map.h"
 #include "pcolor\pcolor.h"
 #include "stack\stack.h"
-#include "ADT Queue/prioqueue.h"
-
-/* blm ditest */
-
-void displayMap(Map m, char loc_mobita, Stack tas, Daftar todo, int nrow, int ncol)
-{
-  /* KAMUS */
-  int row,col;
-  POINT p;
-  char bangunan;
-  ListDin l;
-  /* ALGORITMA */
-  for (row=0; row < nrow+2; row++) {
-    for (col=0; col < ncol+2; col++) {
-      if (row == 0 || row == nrow+1 || col == 0 || col == ncol+1){
-        printf("*");
-        if (col == ncol+1) {
-          printf("\n");
-        }
-      } else {
-        l = LOC(m);
-        p = MakePOINT(row,col);
-        bangunan = findBangunan(l,p);
-        if (bangunan != '-') {
-          if (EQ(ELMTl(l, indexOf(l,loc_mobita)).posisi, p)) {  // lokasi mobita
-            print_yellow(bangunan);
-          } else if (EQ(ELMTl(l,indexOf(l,DropOff(TOP(tas)))).posisi, p)) { // lokasi drop off pesanan
-            print_blue(bangunan);
-          } else if (findOrder(m,todo,p)) { // lokasi pick up pesanan
-            print_red(bangunan);
-          } else if (isAccessible(m,loc_mobita,bangunan)) { // lokasi yang dapat dicapai
-            print_green(bangunan);
-          } else {
-            printf(bangunan);
-          }
-        } else {
-          printf(" ");
-        }
-      }
-    }
-  }
-}
+#include "linkedlist\list_linked.h"
+#include "map\point.h"
+#include "map\listdin.h"
+#include "map\matrix.h"
+#include "node\node.h"
+#include "order\order.h"
+#include <stdio.h>
 
 char findBangunan(ListDin l, POINT p) 
 /* Mencari bangunan dengan posisi p */
@@ -79,22 +44,62 @@ boolean isAccessible(Map m, char loc_mobita, char bangunan)
   return (ELMT(m.mat, idx_mobita, idx_bangunan) == 1);
 }
 
-findOrder(Map m, Daftar todo, POINT p)
+boolean findOrder(Map m, List toDo, POINT p)
 /* Mengembalikan true jika terdapat order dengan lokasi pick up yaitu p */
 {
   /* KAMUS LOKAL */
-  int i, idx_order;
+  int idx_order;
   boolean found;
+  Address loc;
   /* ALGORITMA */
-  i=0;
+  loc = FIRST(toDo);
   found = false;
-  while (i < Qlength(todo) && !found) {
-    idx_order = indexOf(m.lokasi, PickUp(todo.buffer[i]));
+  while (loc != NULL && !found) {
+    idx_order = indexOf(m.lokasi, PickUp(INFO(loc)));
     if (EQ(ELMTl(m.lokasi,idx_order).posisi, p)) {
       found = true;
     } else {
-      i++;
+      loc = NEXT(loc);
     }
   }
   return found;
+}
+
+void displayMap(Map m, char loc_mobita, Stack tas, List toDo, int nrow, int ncol)
+{
+  /* KAMUS */
+  int row,col;
+  POINT p;
+  char bangunan;
+  ListDin l;
+  /* ALGORITMA */
+  for (row=0; row < nrow+2; row++) {
+    for (col=0; col < ncol+2; col++) {
+      if (row == 0 || row == nrow+1 || col == 0 || col == ncol+1){
+        printf("*");
+        if (col == ncol+1) {
+          printf("\n");
+        }
+      } else {
+        l = LOC(m);
+        p = MakePOINT(row,col);
+        bangunan = findBangunan(l,p);
+        if (bangunan != '-') {
+          if (EQ(ELMTl(l, indexOf(l,loc_mobita)).posisi, p)) {  // lokasi mobita
+            print_yellow(bangunan);
+          } else if (EQ(ELMTl(l,indexOf(l,DropOff(TOP(tas)))).posisi, p)) { // lokasi drop off pesanan
+            print_blue(bangunan);
+          } else if (findOrder(m,toDo,p)) { // lokasi pick up pesanan
+            print_red(bangunan);
+          } else if (isAccessible(m,loc_mobita,bangunan)) { // lokasi yang dapat dicapai
+            print_green(bangunan);
+          } else {
+            printf("%c", bangunan);
+          }
+        } else {
+          printf(" ");
+        }
+      }
+    }
+  }
 }
