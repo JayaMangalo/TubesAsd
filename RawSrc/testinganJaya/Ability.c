f11ede4985a43c0c3140ea91e35c10c734a7f24e
Raven <16520022@std.stei.ac.id>
2021-11-18 18:33:00 +0700

    remove ads lagi

--- RawSrc/testinganJaya/Ability.c
+++
@@ -1,6 +0,0 @@
-#include "stack.h"
-#include "prototypeTime.c"
-
-void AddSpeedBoost(Time *T); //udah diimplementasi di prototypeTime.c
-void RemoveSpeedBoost(Time *T); //udah diimplementasi di prototypeTime.c
-void IncreaseCapacity(Stack *s); //udah diimplemntasi di stack.c
