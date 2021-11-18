6ebd3b7f154bfea46afd2822a5ac611ed3e4d1dd
Raven <16520022@std.stei.ac.id>
2021-11-18 18:45:33 +0700

    add time.c

---
+++ src/time/time.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "time.h"
+
+//Create empty time.
+void CreateTime(Time *T){
+    CurrentTime(*T) = 0;
+    Weight(*T) = 0;
+    SpeedBoost(*T) = 0;
+}
+
+//fungsi untuk menambahkan waktu ketika moving 
+//(+1 jika normal, +0.5 jika speedboost +2,3,4 jika ada weight)
+// Juga mengurangi speedboost setiap kali gerak
+// Juga mengupdate time remaining on perishable items (Not implemented yet)
+// Maybe update perishable item
+void AddTimeByMove(Time *T){          
+    if (SpeedBoost(*T) > 0){
+        CurrentTime(*T) = CurrentTime(*T) + 0.5;
+        SpeedBoost(*T)--;
+    }
+    else{
+        CurrentTime(*T) = CurrentTime(*T) + 1 + Weight(*T);
+    }
+}
+
+//fungsi menambahkan time melalui ability speedboost (constant +10 )
+void AddSpeedBoost(Time *T){
+    if (Weight(*T) == 0 ){ 
+        SpeedBoost(*T) = SpeedBoost(*T) +10;
+    }
+}
+
+//fungsi meremove time (jika pickup heavy item)
+void RemoveSpeedBoost(Time *T){
+    SpeedBoost(*T) = 0;
+}
+
+//fungsi sama seperti CurrentTime(T), tetapi hasilnya int dibulatkan kebawah, bukan float.
+int GetCurrentTime(Time T){
+    return (int)CurrentTime(T);
+}
+
